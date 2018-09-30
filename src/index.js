5586f15e9fcff668804916b3c0e9fdbeb1686151
Yengas <yigitcan@hotmail.com.tr>
2018-09-30 10:20:03 +0300

    added socket to store connection error handling.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { fromEvent, from } = require('rxjs');
+const { fromEvent, from, of, NEVER } = require('rxjs');
 const { catchError, distinctUntilChanged, mergeMap, startWith, switchMap, takeUntil } = require('rxjs/operators');
 const bunyan = require('bunyan');
 const config = require('./config');
@@ -14,27 +14,37 @@ async function main(){
 	const MovieDatabaseCreator = await Answers.connect();
 
 	const service$ = socket$.pipe(
-		mergeMap((socket) => {
-			const auth = getAuth(socket);
-			const roomId = getRoomId(socket);
-			logger.info({ auth, roomId }, 'got socket');
-			const mapper = socketMapper(socket, auth);
-			const changeMap$ = fromEvent(socket, EVENTS.ROOM_CHANGE);
-			const disconnect$ = fromEvent(socket, 'disconnect');
+		mergeMap((socket) => of(null).pipe(
+			mergeMap(() => {
+				const auth = getAuth(socket);
+				const roomId = getRoomId(socket);
+				logger.info({ auth, roomId }, 'got socket');
+				const mapper = socketMapper(socket, auth);
+				const changeMap$ = fromEvent(socket, EVENTS.ROOM_CHANGE);
+				const disconnect$ = fromEvent(socket, 'disconnect');
 
-			return changeMap$.pipe(
-				startWith(roomId),
-				distinctUntilChanged(),
-				// for each room change event, re-register to the room/game
-				switchMap((roomId) => {
-					return from(lobby.getOrCreateRoom(roomId, { MovieDatabaseCreator })).pipe(
-						mergeMap(({ action$, state$, dispatch }) => mapper.register(action$, state$, dispatch)),
-						takeUntil(disconnect$)
-					);
-				}),
-				catchError((err) => logger.error(err, 'an error happened in socket mapping'))
-			);
-		}),
+				return changeMap$.pipe(
+					startWith(roomId),
+					distinctUntilChanged(),
+					// for each room change event, re-register to the room/game
+					switchMap((roomId) => {
+						return from(lobby.getOrCreateRoom(roomId, { MovieDatabaseCreator })).pipe(
+							mergeMap(({ action$, state$, dispatch }) => mapper.register(action$, state$, dispatch)),
+							takeUntil(disconnect$)
+						);
+					}),
+					catchError((err) => {
+						logger.error(err, 'an error happened in socket room changing');
+						return NEVER;
+					})
+				);
+			}),
+			catchError((err) => {
+				logger.error(err, 'an error occured when creating socket room mapping');
+				socket.close();
+				return NEVER;
+			})
+		))
 	);
 
 	service$.subscribe(() => null);
